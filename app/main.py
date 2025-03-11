f7ffb39821783ce428c2ee7097eee1100c9cb43b
alaa <alaa1190a@gmail.com>
2025-03-11 16:41:33 +0200

    added an exit option.

--- app/main.py
+++ app/main.py
@@ -2,15 +2,16 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while(True):
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
+        command = input()
 
-    print(f"{command}: command not found")
+        if(command == 'exit 0'):
+            break
+
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
-    while(True):
-        main()
+    main()
