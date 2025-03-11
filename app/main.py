38f3654d0f0e837d19d3922a16a17c98d1000b67
alaa <alaa1190a@gmail.com>
2025-03-11 17:14:56 +0200

    added an echo command.

--- app/main.py
+++ app/main.py
@@ -10,6 +10,15 @@ def main():
         if(command == 'exit 0'):
             break
 
+        parts = command.split()
+
+        if len(parts) > 0 and parts[0] == 'echo':
+            if len(parts) > 1:
+                print(" ".join(parts[1:]))
+            else:
+                print(" ")
+            continue
+
         print(f"{command}: command not found")
 
 
