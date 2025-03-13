571c037ff94a6995f37514ec9278dc9d2c8214bd
alaa <alaa1190a@gmail.com>
2025-03-13 16:18:02 +0200

    updated type to print the path of the non-builtin commands.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import sys
-import shutil
+import os
 
 def main():
+    print("Current PATH:", os.environ["PATH"])
     while(True):
         sys.stdout.write("$ ")
 
@@ -14,33 +15,39 @@ def main():
 
         if len(parts) > 0 and parts[0] == 'echo':
             if len(parts) > 1:
-                excutable_path = find_executable(parts[1])
-                if excutable_path:
-                    print(f"{parts[1]} is {excutable_path}")
-                else:
-                    print(f"{parts[1]}: not found")    
+                print(" ".join(parts[1:]))
             else:
-                print(f"{parts[0]}: command not found")
+                print(" ")
             continue
 
         if len(parts) > 0 and parts[0] == 'type':
             if len(parts) > 1:
-                print(type(parts[1]))
+                print(my_type(parts[1]))
             else:
                 print(f"{parts[0]}: command not found")
             continue
 
         print(f"{command}: command not found")
 
-def type(command):
+def my_type(command):
     defined = ['exit', 'echo', 'type']
+    path = find_executable(command)
     if command in defined:
         return command + ' is a shell builtin'
+    elif path:
+        return command + ' is ' + path
     else:
         return command + ': not found' 
     
 
 def find_executable(command):
-    return shutil.which(command)
+    paths = os.environ.get("PATH", "").split(os.pathsep)
+    for path in paths:
+        executable = os.path.join(path, command)
+        if os.path.isfile(executable) and os.access(executable, os.X_OK):
+            return executable
+    return None
+
+
 if __name__ == "__main__":
     main()
